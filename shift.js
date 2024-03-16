e89b842778d7d359d21f6489bd824461c9e1ab6a
Luana <luanaa.domeneghettii@gmail.com>
2024-03-16 09:40:46 -0300

    Metodo Unshift

---
+++ shift.js
@@ -0,0 +1,25 @@
+//] Metodo shift
+// Remove o primeiro elemento do array
+
+let livros = ['eu e esse meu coração', 'amanhecer', 'o cérebro de um ansioso', 'a cinco passos de você'];
+
+let primeiroLivro = livros.shift();
+
+// 'eu e esse meu coração'
+console.log(primeiroLivro); 
+
+console.log(livros);
+
+// ['amanhecer', 'o cérebro de um ansioso, 'a cinco passos de você']
+
+
+
+let pizzas = ['calabresa', 'peperoni', 'frango', 'atum'];
+
+let primeiraPizza = pizzas.shift();
+
+console.log(primeiraPizza);
+
+console.log(pizzas);
+
+// ['peperoni', 'frango', 'atum']
---
+++ unshift.js
@@ -0,0 +1,21 @@
+// Metodo unshift
+// Adiciona um ou mais elemetos ao início do array
+
+let numeros = [3, 4, 5, 6];
+
+// Utilizando unshift para acrescentar elementos no início do array
+
+numeros.unshift(0, 1, 2);
+
+console.log(numeros);
+
+// [0, 1, 2, 3, 4, 5, 6]
+
+
+let perfumes = ['egeo', 'coffee', 'floratta', 'eudora'];
+
+perfumes.unshift('lancôme', 'paco rabanne', 'carolina herreira');
+
+console.log(perfumes);
+
+// ['egeo', 'coffee', 'floratta', 'eudora', 'lancôme', 'paco rabanne', 'carolina herreira]
