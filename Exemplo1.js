e53fc8ae6744394ae045646cd70783027021814d
Luana <luanaa.domeneghettii@gmail.com>
2024-03-15 21:37:39 -0300

    Primeira versão

---
+++ Exemplo1.js
@@ -0,0 +1,17 @@
+// Iniciando a atividade
+
+//Metodo 1 : push
+//push: adiciona na ultima pos
+let cestaFrutas = [ 'goiaba', 'uva', 'pera'];
+
+// Adiciona 'abacaxi' no final
+cestaFrutas.push('abacaxi'); 
+
+// ['goiaba', 'uva', 'abacaxi']
+console.log(cestaFrutas); 
+
+// Adiciona 'caju' e 'amora' no final 
+cestaFrutas.push('caju', 'amora');
+
+// ['goiaba', 'uva', 'pera', 'abacaxi', 'caju', 'amora']
+console.log(cestaFrutas);
