25b6178dc73a0d2c3fd21a5b3651688ee360b515
Luana <luanaa.domeneghettii@gmail.com>
2024-03-16 12:25:04 -0300

    Metodo Reverse

---
+++ reverse.js
@@ -0,0 +1,18 @@
+// Metodo reverse()
+// Inverte o array
+
+let salgados = ['bolinha de queijo', 'croquete', 'pastelzinho', 'coxinha', 'enroladodinho de salsicha'];
+
+salgados.reverse();
+
+console.log(salgados);
+
+// ['enroladodinho de salsicha', 'coxinha', 'pastelzinho', 'croquete', 'bolinha de queijo']
+
+
+let brinquedos = ['carrinho', 'bola', 'boneca', 'lego', 'amoeba'];
+
+brinquedos.reverse();
+
+console.log(brinquedos);
+
