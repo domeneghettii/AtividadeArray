d78bfcc56e32dffc88ede462a01a6792cb8270ef
Luana <luanaa.domeneghettii@gmail.com>
2024-03-16 10:47:21 -0300

    Metodo Splice

---
+++ splice.js
@@ -0,0 +1,20 @@
+// Metodo splice
+// Permite adicionar, remover ou substituir elementos em qualquer posição do array
+
+let frutas = ['banana', 'goiaba', 'amexa', 'manga'];
+
+frutas.splice(3, 1);
+// Vai remover o item 
+
+console.log(frutas);
+
+// ['banana', 'goiaba', 'amexa'] 
+
+
+let nomes =['luana', 'victória', 'nicole', 'renan', 'érika'];
+
+nomes.splice(2, 4);
+
+console.log(nomes);
+
+//  ['luana', 'victória', ']
