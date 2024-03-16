e4bcae51e328c43af75d6ad37599c73114cc50b9
Luana <luanaa.domeneghettii@gmail.com>
2024-03-16 11:42:30 -0300

    Metodo indexOf

---
+++ indexOf.js
@@ -0,0 +1,18 @@
+// Metodo indexOf()
+//  Retorna o primeiro índice no elemento encontrado no array
+
+let doces = ['chocolate', 'paçoca', 'pirulito', 'bala'];
+
+let indiceBala = doces.indexOf('paçoca');
+
+console.log(indiceBala);
+
+// aparece o 1, pois o elemento 1 é a 'paçoca' que está dentro do array
+
+
+let indiceAlfajor = doces.indexOf('alfajor');
+
+console.log(indiceAlfajor);
+
+// aparece -1, pois o elemento 'alfajor' bnão está dentro do array
+
