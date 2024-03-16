103c88f0eae3fc36d04b91f2050d2eeb4e8afe40
Luana <luanaa.domeneghettii@gmail.com>
2024-03-15 22:12:38 -0300

    Metodo Pop

---
+++ pop.js
@@ -0,0 +1,17 @@
+//metodo 2 : pop
+//pop : remove o ultimo elemento
+
+let animais = ['gato', 'cachorro','tigre', 'papagaio' ];
+
+animais.pop();
+
+console.log (animais);
+
+
+let cores= ['preto', 'branco','azul', 'verde' ];
+
+cores.pop();
+
+console.log (cores);
+
+// ['preto', 'branco', 'azul']
